3566e4a419b8af3e440a206e6745af9cf9029cfe
ssareth <66816067+ssareth@users.noreply.github.com>
2021-04-02 12:42:08 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function increment()
+    {
+        var number = Number(document.getElementById('number').innerHTML)
+        document.getElementById('number').innerHTML = number +1
+    }
+
+function decrement()
+
+
+    {
+        var number = Number(document.getElementById('number').innerHTML)
+        document.getElementById('number').innerHTML = number -1
+    }
+
+// function increment()
+//     {
+//         var number = Number(document.getElementById('number').innerHTML)
+//         document.getElementById('number').innerHTML = number +1
+//     }
+
+function neutral(){
+
+ 
+        document.getElementById('number').innerHTML = 0
+   
+
+}
